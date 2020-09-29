d4e67ebaddbf3634dd4ffaf278e92816ef78e35e
Sophia Peckner <sophiapeckner@gmail.com>
2020-09-28 20:00:42 -0700

    integrate html and js

--- draft1.js
+++ draft1.js
@@ -1,4 +1,8 @@
-function calculateCohort(maxCohortSize, totalStudents){
+
+var classAssignments = [];
+
+  
+function calculateCohort(classRoom, maxCohortSize, totalStudents){
   var howManyCohort = totalStudents / maxCohortSize;
   if (Number.isInteger(howManyCohort) === true){
     var cohortSize = maxCohortSize;
@@ -10,15 +14,31 @@ function calculateCohort(maxCohortSize, totalStudents){
     } else {
       var remainder = totalStudents % unevenHowManyCohort;
       var lastCohortSize = Math.floor(intCohortSize) + remainder;
-      console.log(lastCohortSize);
+      // lastCohortSize printing out!
+      console.log(classRoom + "_" + lastCohortSize);
+      classAssignments.push(classRoom + "_" + lastCohortSize);
       var remainingCohortSize = Math.floor(intCohortSize);
       var howManyCohort = howManyCohort - 1;
       var cohortSize = remainingCohortSize;
     };
   };
   for(var i = 0; i < howManyCohort; i++){
-      console.log(cohortSize);
+      console.log(classRoom + "_" + cohortSize);
+      classAssignments.push(classRoom + "_" + cohortSize);
   };
+  return classAssignments;
 };
 
-calculateCohort(11, 21);
+var reopenForm = document.getElementById("reopenForm");
+// if (reopenForm) {
+  reopenForm.onsubmit = function () {
+    // cohortSizes.value = calculateCohort("a", cohortLimit.value,classA.value);
+    document.getElementById("cohortSizes").innerHTML = calculateCohort("a", cohortLimit.value,classA.value);
+    // console.log(this.cohortSizes.value);
+    return false;
+  };
+// }
+
+// calculateCohort("a", 12, 28);
+// calculateCohort("b", 11, 20);
+// console.log(classAssignments);
