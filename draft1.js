420345c595ff99b6f776f8a57a0832ff4779400f
Sophia Peckner <sophiapeckner@gmail.com>
2020-10-04 16:59:42 -0700

    push all classes to array

--- draft1.js
+++ draft1.js
@@ -1,9 +1,10 @@
 // CREDIT:
-// https://www.sitepoint.com/community/t/math-formula-input-form/6956/3
+//https://www.sitepoint.com/community/t/math-formula-input-form/6956/3
+//http://jsfiddle.net/t656N/1/
 
 var classAssignments = [];
   
-function calculateCohort(classRoom, maxCohortSize, totalStudents){
+function calculateCohort(classRoom, maxCohortSize, totalStudents, grade){
   var howManyCohort = totalStudents / maxCohortSize;
   if (Number.isInteger(howManyCohort) === true){
     var cohortSize = maxCohortSize;
@@ -16,60 +17,44 @@ function calculateCohort(classRoom, maxCohortSize, totalStudents){
       var remainder = totalStudents % unevenHowManyCohort;
       var lastCohortSize = Math.floor(intCohortSize) + remainder;
       // lastCohortSize printing out!
-      console.log(classRoom + "_" + lastCohortSize);
-      classAssignments.push(classRoom + "_" + lastCohortSize);
+      console.log(classRoom + "_" + lastCohortSize + "_" + grade);
+      classAssignments.push(classRoom + "_" + lastCohortSize + "_" + grade);
       var remainingCohortSize = Math.floor(intCohortSize);
       var howManyCohort = howManyCohort - 1;
       var cohortSize = remainingCohortSize;
     };
   };
   for(var i = 0; i < howManyCohort; i++){
-      console.log(classRoom + "_" + cohortSize);
-      classAssignments.push(classRoom + "_" + cohortSize);
+      console.log(classRoom + "_" + cohortSize + "_" + grade);
+      classAssignments.push(classRoom + "_" + cohortSize + "_" + grade);
   };
   return classAssignments;
 };
 
-var reopenForm = document.getElementById("reopenForm");
-// if (reopenForm) {
-reopenForm.onsubmit = function () {
-  cohortSizes.value = calculateCohort("a", cohortLimit.value,classA.value);
-  // document.getElementById("cohortSizes").innerHTML = calculateCohort("a", cohortLimit.value,classA.value);
-  // console.log(this.cohortSizes.value);
-  return false;
-};
-// }
-
-function addField(){
-  var howManyClasses = document.getElementById("howManyClasses").value;
+function addField(grade){
+  grade = "grade1";
+  window.howManyClasses = document.getElementById("howManyClasses").value;
   var classFields = document.getElementById("classFields");
   while(classFields.hasChildNodes()){
     classFields.removeChild(classFields.lastChild);
   };
   for (i = 0; i < howManyClasses; i++){
     classFields.appendChild(document.createTextNode("Class " + (i + 1 )));
-    classFields.id = i + 1;
-    // console.log(classFields.id);
-    var input = document.createElement("input");
+    window.input = document.createElement("input");
+    input.id = (i + 1) + "_" + grade;
     input.type = "number";
     classFields.appendChild(input);
+  
+    classFields.appendChild(document.createElement("br"));
 
-    classFields.appendChild(document.createElement("br"));
-    classFields.appendChild(document.createElement("br"));
   };
 };
 
-
-// function addField(){
-//   var classFields = document.getElementById("classFields");
-//   var currentFields = classFields.childElementCount;
-//   var newFields = document.createElement("INPUT");
-//   newFields.setAttribute("type", "number");
-//   newFields.setAttribute("value", (currentFields + 1));
-//   newFields.setAttribute("id", (currentFields + 1));
-//   classFields.appendChild(newFields);
-// }
-
-// calculateCohort("a", 12, 28);
-// calculateCohort("b", 11, 20);
-// console.log(classAssignments);
+var reopenForm = document.getElementById("reopenForm");
+reopenForm.onsubmit = function () {
+  for (i = 0; i < howManyClasses; i++){
+    var x = document.getElementById((i + 1) + "_" + "grade1");
+    cohortSizes.value = calculateCohort((i + 1), cohortLimit.value, x.value, "grade1");
+  };
+  return false;
+};
